8a94c2951e653be78ff9ab7d7b8adcea0eab5561
Erik Stielstra <info@erikstielstra.nl>
2014-09-02 08:33:49 +0200

    Add example block template override.

--- thema/Stap 6 - preprocess-functies/base2013/templates/block.tpl.php
+++ thema/Stap 6 - preprocess-functies/base2013/templates/block.tpl.php
@@ -47,15 +47,15 @@
  * - Title tag changed to <h3>
  */
 ?>
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<aside id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
 <?php if ($block->subject): ?>
-  <h3<?php print $title_attributes; ?>><?php print $block->subject ?></h3>
+  <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
 <?php endif;?>
   <?php print render($title_suffix); ?>
 
   <div class="content"<?php print $content_attributes; ?>>
     <?php print $content ?>
   </div>
-</div>
+</aside>
